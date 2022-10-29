2e1a05a673ce63c5a4170932d2a8c0c06e588884
Romashka <rroma13579@gmail.com>
2022-10-29 18:28:28 +0200

    eleventh commit

--- js/script.js
+++ js/script.js
@@ -21,6 +21,20 @@ colors.forEach(color => {
     });
 });
 
+var headerSizes = document.querySelector('.header-sizes');
+var sizes = document.querySelectorAll('.size');
+var size = 2;
+for (let k = 0; k < sizes.length; k++) {
+    sizes[k].addEventListener('click', () =>{
+        size = (k+1)*2;
+        console.log(size);
+        square.style.borderWidth = ""+size+"px";
+    });
+}
+
+
+
+
 var mainBlock1 = document.querySelector('.main-block-1');
 var square = document.querySelector('.square');
 
@@ -42,39 +56,27 @@ mainBlock1.addEventListener('mousemove', (e) => {
         var y2 = e.clientY;
         square.style.display = "block";
         if(x2>=x1){
-            if(square.className == "line"){
-                square.style.clipPath = "polygon(0 1px, 1px 0, 100% calc(100% - 1px), calc(100% - 1px) 100%)";
-            }
             square.style.width = "" + (x2 - x1) + "px";
         }
         else if(x2<x1){
-            if(square.className == "line"){
-                square.style.clipPath = "polygon(calc(100% - 1px) 0, 100% 1px, 1px 100%, 0 calc(100% - 1px))";
-            }
             square.style.left = "" + ((x1) - (x1 - x2)) + "px";
             square.style.width = "" + (x1 - x2) + "px";
         }
 
         if(y2>=y1){
-            if(square.className == "line"){
-                square.style.clipPath = "polygon(0 1px, 1px 0, 100% calc(100% - 1px), calc(100% - 1px) 100%)";
-            }
             square.style.height = "" + (y2 - y1) + "px";
         }
         else if(y2<y1){
-            if(square.className == "line"){
-                square.style.clipPath = "polygon(calc(100% - 1px) 0, 100% 1px, 1px 100%, 0 calc(100% - 1px))";
-            }
             square.style.top = "" + ((y1) - (y1 - y2)) + "px";
             square.style.height = "" + (y1 - y2) + "px";
         }
         
         if(square.className == "line"){
             if((x2>=x1 && y2>=y1) || (y2<y1 && x2<x1)){
-                square.style.clipPath = "polygon(0 1px, 1px 0, 100% calc(100% - 1px), calc(100% - 1px) 100%)";
+                square.style.clipPath = "polygon(0 "+size+"px, "+size+"px 0, 100% calc(100% - "+size+"px), calc(100% - "+size+"px) 100%)";
             }
             else if((x2>=x1 && y2<y1) || (y2>=y1 && x2<x1)){
-                square.style.clipPath = "polygon(calc(100% - 1px) 0, 100% 1px, 1px 100%, 0 calc(100% - 1px))";
+                square.style.clipPath = "polygon(calc(100% - "+size+"px) 0, 100% "+size+"px, "+size+"px 100%, 0 calc(100% - "+size+"px))";
             }
             else if(y2<y1 && x2<x1){
                 square.style.clipPath = "polygon(0 1px, 1px 0, 100% calc(100% - 1px), calc(100% - 1px) 100%)";
