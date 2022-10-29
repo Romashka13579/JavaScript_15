b3b001b17845d37f09038aa2f677e3d06de9fd51
Romashka <rroma13579@gmail.com>
2022-10-29 18:13:30 +0200

    tenth commit

--- js/script.js
+++ js/script.js
@@ -8,10 +8,16 @@ for (let k = 0; k < colorArray.length; k++) {
     headerColors.append(colorclone);
 }
 
+var line = document.querySelector('.line');
 var colors = document.querySelectorAll('.color');
 colors.forEach(color => {
     color.addEventListener('click', () => {
-        square.style.borderColor = color.style.background;
+        if (square.className == "line") {
+            square.style.background = color.style.background;
+        }
+        else{
+            square.style.borderColor = color.style.background;
+        }
     });
 });
 
@@ -36,17 +42,46 @@ mainBlock1.addEventListener('mousemove', (e) => {
         var y2 = e.clientY;
         square.style.display = "block";
         if(x2>=x1){
+            if(square.className == "line"){
+                square.style.clipPath = "polygon(0 1px, 1px 0, 100% calc(100% - 1px), calc(100% - 1px) 100%)";
+            }
             square.style.width = "" + (x2 - x1) + "px";
         }
         else if(x2<x1){
+            if(square.className == "line"){
+                square.style.clipPath = "polygon(calc(100% - 1px) 0, 100% 1px, 1px 100%, 0 calc(100% - 1px))";
+            }
             square.style.left = "" + ((x1) - (x1 - x2)) + "px";
             square.style.width = "" + (x1 - x2) + "px";
         }
 
-        if(y2>=y1){square.style.height = "" + (y2 - y1) + "px";}
+        if(y2>=y1){
+            if(square.className == "line"){
+                square.style.clipPath = "polygon(0 1px, 1px 0, 100% calc(100% - 1px), calc(100% - 1px) 100%)";
+            }
+            square.style.height = "" + (y2 - y1) + "px";
+        }
         else if(y2<y1){
+            if(square.className == "line"){
+                square.style.clipPath = "polygon(calc(100% - 1px) 0, 100% 1px, 1px 100%, 0 calc(100% - 1px))";
+            }
             square.style.top = "" + ((y1) - (y1 - y2)) + "px";
             square.style.height = "" + (y1 - y2) + "px";
+        }
+        
+        if(square.className == "line"){
+            if((x2>=x1 && y2>=y1) || (y2<y1 && x2<x1)){
+                square.style.clipPath = "polygon(0 1px, 1px 0, 100% calc(100% - 1px), calc(100% - 1px) 100%)";
+            }
+            else if((x2>=x1 && y2<y1) || (y2>=y1 && x2<x1)){
+                square.style.clipPath = "polygon(calc(100% - 1px) 0, 100% 1px, 1px 100%, 0 calc(100% - 1px))";
+            }
+            else if(y2<y1 && x2<x1){
+                square.style.clipPath = "polygon(0 1px, 1px 0, 100% calc(100% - 1px), calc(100% - 1px) 100%)";
+            }
+        }
+        else{
+            square.style.clipPath = "none";
         }
     }
 });
@@ -72,10 +107,13 @@ console.log(circled);
 
 squared.addEventListener('click', () => {
     square.className = "square";
+    square.style.background = "none";
 });
 circled.addEventListener('click', () => {
     square.className = "circle";
+    square.style.background = "none";
 });
 lined.addEventListener('click', () => {
     square.className = "line";
+    square.style.background = "black";
 });
